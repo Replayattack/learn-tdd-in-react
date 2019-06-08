d5283134e7a15cf5b989e6ac1b38564db5e66ac2
Daniel G <replayattack@pm.me>
2019-06-07 23:33:46 -0500

    Add the NewMessageForm component behavior to pass the unit tests

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 
 export default class NewMessageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { inputText: '' }
+
+    this.handleTextChange = this.handleTextChange.bind(this)
+    this.handleSend = this.handleSend.bind(this)
+  }
+
+  handleTextChange({ target: { value: inputText } }) {
+    this.setState({
+      inputText
+    })
+  }
+
+  handleSend = () => {
+    this.setState({ inputText: '' })
+  }
+
   render() {
+    const { inputText } = this.state
     return (
       <div>
-        <input type="text" data-testId="messageText"/>
-        <button data-testId="sendButton" >Send</button>
+        <input
+          type="text"
+          data-testId="messageText"
+          value={inputText}
+          onChange={this.handleTextChange}
+        />
+        <button
+          data-testId="sendButton"
+          onClick={this.handleSend}
+        >
+          Send
+        </button>
       </div>
     )
   }
